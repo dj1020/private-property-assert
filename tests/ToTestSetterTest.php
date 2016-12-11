3834b76a9dbcdf709187329c5a07f9c64c2999bc
Ken Lin <kenlin@kkbox.com>
2016-12-11 13:53:44 +0800

    Done test assert a private property of a class

--- tests/ToTestSetterTest.php
+++ tests/ToTestSetterTest.php
@@ -10,9 +10,19 @@ class ToTestSetterTest extends PHPUnit_Framework_TestCase
     }
 
     /** @test */
-    function it_should_instaniate_ToTestSetter_class() {
+    function it_should_instantiate_ToTestSetter_class() {
         $target = new ToTestSetter();
 
         $this->assertInstanceOf(ToTestSetter::class, $target);
+    }
+
+    /** @test */
+    function it_should_be_able_to_assert_a_private_property_in_object() {
+        $target = new ToTestSetter();
+
+        $target->setName('Ken');
+
+        $this->assertObjectHasAttribute('name', $target);
+        $this->assertAttributeEquals('Ken', 'name', $target);
     }
 }

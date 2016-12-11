1d3b12e403d2bd3294c698d627c3b93178ad748b
Ken Lin <kenlin@kkbox.com>
2016-12-11 13:50:43 +0800

    Done 2nd test case to instanticate a class

--- tests/ToTestSetterTest.php
+++ tests/ToTestSetterTest.php
@@ -1,9 +1,18 @@
 <?php
 
+use App\ToTestSetter;
+
 class ToTestSetterTest extends PHPUnit_Framework_TestCase
 {
     /** @test */
     function it_should_be_true() {
         $this->assertTrue(true);
+    }
+
+    /** @test */
+    function it_should_instaniate_ToTestSetter_class() {
+        $target = new ToTestSetter();
+
+        $this->assertInstanceOf(ToTestSetter::class, $target);
     }
 }
